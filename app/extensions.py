3964850cd1af44f8472fd03baff823b2ce56cbdf
gaabibmatos <gaabibmatos@gmail.com>
2026-02-23 16:42:31 -0400

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -2,5 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
+
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
+login_manager.login_message = "Faça login para acessar."
+login_manager.login_message_category = "warning"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    try:
+        return User.query.get(int(user_id))
+    except Exception:
+        return None
