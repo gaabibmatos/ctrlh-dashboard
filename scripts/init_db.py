237b1adb24455cad3664edc27fd83f36aac89d54
gaabibmatos <gaabibmatos@gmail.com>
2026-02-23 15:47:21 -0400

    Update init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,14 +1,12 @@
 import os
 import sys
 
-# garante que a raiz do projeto esteja no PYTHONPATH
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
 from app import create_app
-from app.extensions import db
-from app.db import init_db
+from app.db_init import init_db   # <-- TROCOU AQUI
 
 app = create_app()
 
