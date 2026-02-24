224561be3117e1c493f5cc540748443e575271a5
gaabibmatos <gaabibmatos@gmail.com>
2026-02-24 12:05:54 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,12 @@
-import os
 from flask import Flask
-from dotenv import load_dotenv
 
+from .config import Config
 from .extensions import db, login_manager
-from .utils import build_db_uri
-
-load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")
+    app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -18,11 +14,10 @@ def create_app():
     # Blueprints
     from .routes.auth import bp as auth_bp
     from .routes.dashboard import bp as dashboard_bp
-    from .routes.finance import bp as finance_bp  # <<<
-    # (se tiver supply/ops/performance, mantém também)
+    from .routes.finance import bp as finance_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
-    app.register_blueprint(finance_bp)  # <<<
+    app.register_blueprint(finance_bp)
 
     return app
